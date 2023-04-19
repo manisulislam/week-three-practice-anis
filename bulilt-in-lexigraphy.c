738807e18bef94d2bae30a0a9d9be004dff826ca
anisul islam <anisulislam980340@gmail.com>
2023-04-19 15:46:30 +0600

    logically solve concatination

---
+++ bulilt-in-lexigraphy.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+  char a[100], b[100]        ;
+  scanf("%s %s", a, b);
+  int v= strcmp(a,b);
+  if(v<0){
+    printf("B choto\n");
+
+  }
+  else if(v>0){
+    printf(" A choto\n");
+  }
+  else{
+    printf("same\n");
+  }
+  return 0;
+}
